4da2e4c588c6b96a59b99349903e10b71e21bd5b
Asi Baka <39007861+asabaki@users.noreply.github.com>
2019-05-27 23:50:57 +0530

    Config environment

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoUri: process.env.mongoUri,
+    cookieKey: process.env.cookieKey,
+    googleApi: process.env.googleApi,
+    channelAccessToken: process.env.channelAccessToken,
+    channelSecret: process.env.channelSecret
+};
