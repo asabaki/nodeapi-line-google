8ab402b980beaf70a6d858fada5cfc811e0ddf62
Asi Baka <39007861+asabaki@users.noreply.github.com>
2019-05-29 02:07:26 +0700

    Update Chatbot

--- controllers/SCG.js
+++ controllers/SCG.js
@@ -98,86 +98,8 @@ exports.replyNow = (req, res, next) => {
     //     const action = event.postback.data;
     //
     // }
-    if (event.type === 'location') {
-        const lat = event.message.latitude;
-        const lon = event.message.longitude;
-        const options = {
-            url: `https://api.openweathermap.org/data/2.5/weather`,
-            qs: {
-                APPID: '50e72c3fdbf1a59902613f027a11a64c',
-                lon,
-                lat,
-                units: 'metric'
-            }
-        };
-        request(options, (err, response, body) => {
-            const body_json = JSON.parse(body);
-            const climate = body_json.weather[0].main;
-            const temp = body_json.main.temp;
-            if (climate === 'Thunderstorm') {
-                client.replyImage(token, {
-                    originalContentUrl: 'https://imgbbb.com/images/2019/05/28/thunder.jpg',
-                    previewImageUrl: 'https://imgbbb.com/images/2019/05/28/thunder.th.jpg'
-                });
-                client.reply(token, {
-                    type: 'text',
-                    text:`Whoa, It seems that there is a thunderstorm out there. The temperature right now is ${temp} °C`
-                })
-            }
-            if (climate === 'Clouds') {
-                client.replyImage(token, {
-                    originalContentUrl: 'https://imgbbb.com/images/2019/05/28/cloudy.jpg',
-                    previewImageUrl: 'https://imgbbb.com/images/2019/05/28/cloudy.th.jpg'
-                });
-                client.reply(token, {
-                    type: 'text',
-                    text:`Hmmm, Quite Cloudy today. The temperature right now is ${temp} °C`
-                })
-            }
-            if (climate === 'Drizzle') {
-                client.replyImage(token, {
-                    originalContentUrl: 'https://imgbbb.com/images/2019/05/28/drizzle.jpg',
-                    previewImageUrl: 'https://imgbbb.com/images/2019/05/28/drizzle.th.jpg'
-                });
-                client.reply(token, {
-                    type: 'text',
-                    text:`Drizzling today huh? The temperature right now is ${temp} °C`
-                })
-            }
-            if (climate === 'Rain') {
-                client.replyImage(token, {
-                    originalContentUrl: 'https://imgbbb.com/images/2019/05/28/rain.jpg',
-                    previewImageUrl: 'https://imgbbb.com/images/2019/05/28/rain.th.jpg'
-                });
-                client.reply(token, {
-                    type: 'text',
-                    text:`Don\'t get wet today ain\'t you?. The temperature right now is ${temp} °C`
-                })
-            }
-            if (climate === 'Snow') {
-                client.replyImage(token, {
-                    originalContentUrl: 'https://imgbbb.com/images/2019/05/28/snow.jpg',
-                    previewImageUrl: 'https://imgbbb.com/images/2019/05/28/snow.th.jpg'
-                });
-                client.reply(token, {
-                    type: 'text',
-                    text:`Bruh It\'s cold out there. The temperature right now is ${temp} °C`
-                })
-            }
-            if (climate === 'Clear') {
-                client.replyImage(token, {
-                    originalContentUrl: 'https://imgbbb.com/images/2019/05/28/sunny.jpg',
-                    previewImageUrl: 'https://imgbbb.com/images/2019/05/28/sunny.th.jpg'
-                });
-                client.reply(token, {
-                    type: 'text',
-                    text:`Yeah! very sunny today. The temperature right now is ${temp} °C`
-                })
-            }
-            res.json(JSON.parse(body));
-        });
-        console.log(event)
-    } else {
+    console.log('Type: ', event.type);
+    if (event.type === 'message') {
         client.replyCarouselTemplate(token, 'Forecast', [
             {
                 thumbnailImageUrl: 'https://images.unsplash.com/photo-1545259742-b4fd8fea67e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
@@ -214,6 +136,87 @@ exports.replyNow = (req, res, next) => {
             //     ],
             // },
         ]);
+    } else {
+        {
+            const lat = event.message.latitude;
+            const lon = event.message.longitude;
+            const options = {
+                url: `https://api.openweathermap.org/data/2.5/weather`,
+                qs: {
+                    APPID: '50e72c3fdbf1a59902613f027a11a64c',
+                    lon,
+                    lat,
+                    units: 'metric'
+                }
+            };
+            request(options, (err, response, body) => {
+                const body_json = JSON.parse(body);
+                const climate = body_json.weather[0].main;
+                const temp = body_json.main.temp;
+                if (climate === 'Thunderstorm') {
+                    client.replyImage(token, {
+                        originalContentUrl: 'https://imgbbb.com/images/2019/05/28/thunder.jpg',
+                        previewImageUrl: 'https://imgbbb.com/images/2019/05/28/thunder.th.jpg'
+                    });
+                    client.reply(token, {
+                        type: 'text',
+                        text:`Whoa, It seems that there is a thunderstorm out there. The temperature right now is ${temp} °C`
+                    })
+                }
+                if (climate === 'Clouds') {
+                    client.replyImage(token, {
+                        originalContentUrl: 'https://imgbbb.com/images/2019/05/28/cloudy.jpg',
+                        previewImageUrl: 'https://imgbbb.com/images/2019/05/28/cloudy.th.jpg'
+                    });
+                    client.reply(token, {
+                        type: 'text',
+                        text:`Hmmm, Quite Cloudy today. The temperature right now is ${temp} °C`
+                    })
+                }
+                if (climate === 'Drizzle') {
+                    client.replyImage(token, {
+                        originalContentUrl: 'https://imgbbb.com/images/2019/05/28/drizzle.jpg',
+                        previewImageUrl: 'https://imgbbb.com/images/2019/05/28/drizzle.th.jpg'
+                    });
+                    client.reply(token, {
+                        type: 'text',
+                        text:`Drizzling today huh? The temperature right now is ${temp} °C`
+                    })
+                }
+                if (climate === 'Rain') {
+                    client.replyImage(token, {
+                        originalContentUrl: 'https://imgbbb.com/images/2019/05/28/rain.jpg',
+                        previewImageUrl: 'https://imgbbb.com/images/2019/05/28/rain.th.jpg'
+                    });
+                    client.reply(token, {
+                        type: 'text',
+                        text:`Don\'t get wet today ain\'t you?. The temperature right now is ${temp} °C`
+                    })
+                }
+                if (climate === 'Snow') {
+                    client.replyImage(token, {
+                        originalContentUrl: 'https://imgbbb.com/images/2019/05/28/snow.jpg',
+                        previewImageUrl: 'https://imgbbb.com/images/2019/05/28/snow.th.jpg'
+                    });
+                    client.reply(token, {
+                        type: 'text',
+                        text:`Bruh It\'s cold out there. The temperature right now is ${temp} °C`
+                    })
+                }
+                if (climate === 'Clear') {
+                    client.replyImage(token, {
+                        originalContentUrl: 'https://imgbbb.com/images/2019/05/28/sunny.jpg',
+                        previewImageUrl: 'https://imgbbb.com/images/2019/05/28/sunny.th.jpg'
+                    });
+                    client.reply(token, {
+                        type: 'text',
+                        text:`Yeah! very sunny today. The temperature right now is ${temp} °C`
+                    })
+                }
+                res.json(JSON.parse(body));
+            });
+            console.log(event)
+        }
     }
 
 
